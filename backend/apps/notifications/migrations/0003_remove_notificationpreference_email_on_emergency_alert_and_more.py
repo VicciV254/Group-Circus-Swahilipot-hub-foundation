c2740b4d5d7d822b64b77cd82155b4e3d0a2f8ca
David <ochiengsdavids@gmail.com>
2026-06-21 23:38:13 +0300

    Update 0003_remove_notificationpreference_email_on_emergency_alert_and_more.py

--- backend/apps/notifications/migrations/0003_remove_notificationpreference_email_on_emergency_alert_and_more.py
+++ backend/apps/notifications/migrations/0003_remove_notificationpreference_email_on_emergency_alert_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('notifications', '0002 fm notification fields'),
+        ('notifications', '0002_fm_notification_fields'),
     ]
 
     operations = [
